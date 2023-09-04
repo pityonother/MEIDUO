f8078b0e9807f096cdde951d6575a7453d23a65e
CY <986160062@qq.com>
2023-09-04 21:04:17 +0800

    绑定submit和crfs事件

--- meiduo_mail/apps/users/views.py
+++ meiduo_mail/apps/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 from django.views import View
@@ -6,4 +7,6 @@ class RegisterView(View):
     def get(self,request):
         return render(request,'register.html')
     def post(self,request):
-        pass
+        print('hellp')
+
+        return HttpResponse("Post request received.")
