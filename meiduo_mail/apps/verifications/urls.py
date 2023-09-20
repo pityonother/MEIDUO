3ce48c174fca02111a7d19c359e57142b09bef3f
CY <986160062@qq.com>
2023-09-20 08:59:45 +0800

    实现短信发送的后端逻辑

--- meiduo_mail/apps/verifications/urls.py
+++ meiduo_mail/apps/verifications/urls.py
@@ -2,4 +2,5 @@ from django.urls import re_path
 from . import views
 urlpatterns=[
     re_path(r'^image_codes/(?P<uuid>[\w-]+)/$',views.ImageCodeView.as_view()),
+    re_path(r'^sms_codes/(?P<mobile>1[3-9]\d{9}/$)',views.SmsCodeView.as_view(),name='smscode'),
 ]
--- meiduo_mail/apps/verifications/views.py
+++ meiduo_mail/apps/verifications/views.py
@@ -5,10 +5,32 @@ from django.views import View
 from libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
 from django import http
-from .constants import IMAGE_CODE_EXPIRE_TIME
+from .constants import IMAGE_CODE_EXPIRE_TIME,SMS_CODE_EXPIRE_TIME
+from utils.response_code import RETCODE
+from libs.yuntongxun.sms import CCP
 class ImageCodeView(View):
     def get(self,request,uuid):
         text,image=captcha.generate_captcha()
         redis_Conn=get_redis_connection('code')
         redis_Conn.setex('img_%s'%uuid,IMAGE_CODE_EXPIRE_TIME,text)
         return http.HttpResponse(image,content_type='image/jpeg')
+
+class SmsCodeView(View):
+    def get(self,request,mobile):
+        image_code=request.GET.get('imagecode')
+        uuid=request.GET.get('uuid')
+        if not all([mobile,image_code,uuid]):
+            return http.JsonResponse({'code':RETCODE.NECESSARYPARAMERR,'errormsg':'参数不齐'})
+        redis_conn=get_redis_connection('code')
+        redis_code=redis_conn.get('img_%s'%mobile)
+        if redis_code is None:
+            return http.JsonResponse({'code':RETCODE.Timeout,'errmsg':'验证码过期'})
+        if redis_code!=image_code:
+            return  http.JsonResponse({'code':RETCODE.SMSCODERR,'errmsg':'图片验证码错误'})
+        from random import randint
+        sms_code=randint(1000,9999)
+        redis_conn.setex('sms_%s'%mobile,SMS_CODE_EXPIRE_TIME,sms_code)
+        CCP().send_template_sms(mobile,[sms_code,5],1)
+        return http.JsonResponse({'code':RETCODE.OK,'errmsg':'ok'})
+
+
